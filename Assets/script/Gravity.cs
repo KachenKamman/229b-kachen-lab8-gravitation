6a3c205fcb21fa9255d4c4c8b7c738a94c8fd862
KachenKamman <kachen.kamm@bumail.net>
2025-03-10 16:28:19 +0700

    script completed

--- Assets/script/Gravity.cs
+++ Assets/script/Gravity.cs
@@ -1,16 +1,46 @@
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   Rigidbody rb;
+   const float G = 0.006674f;
+   public static List<Gravity> planetLists;
+   
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Awake()
+   {
+      rb = GetComponent<Rigidbody>();
+      if (planetLists == null)
+      {
+         planetLists = new List<Gravity>();
+      }
+      planetLists.Add(this);
+   }
+   
+   private void FixedUpdate()
+   {
+      foreach (var Planet in planetLists)
+      {
+         if(Planet != this)
+             Attract(Planet);
+      }
+   }
+
+   void Attract(Gravity other)
+   {
+      Rigidbody otherRb = other.rb;
+
+      Vector3 direction = rb.position - otherRb.position;
+
+      float distance = direction.magnitude;
+
+
+      float forceMagnitude = G * (rb.mass * otherRb.mass) / Mathf.Pow(distance, 2);
+      Vector3 finalForce = forceMagnitude * direction.normalized;
+      
+      otherRb.AddForce(finalForce);
+   }
 }
